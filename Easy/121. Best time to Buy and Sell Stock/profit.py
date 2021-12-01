8db5be0bfa1884773bc685a94583e80ee0aa2eb4
Nate Pierce <nate.pierce@maymobility.com>
2021-12-01 11:34:43 -0500

    Completed 121 in O(nlogn).

--- Easy/121. Best time to Buy and Sell Stock/profit.py
+++ Easy/121. Best time to Buy and Sell Stock/profit.py
@@ -10,22 +10,37 @@ Output: 5
 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
+Constraints:
+
+1 <= prices.length <= 10E5
+0 <= prices[i] <= 10E4
+
 """
-# not optimal
+# not optimal - O(nlogn)
 class Solution:
     def maxProfit(self, prices) -> int:
         min = 10E4
         max = 0
+        profit = 0
         n = len(prices)
 
-        for i in range(0, n):
+        if n == 0 or n == 1: # check for empty price list, or only one price
+            return 0
+
+        # This function runs in O(nlogn) due to this for loop. 
+        # We want to exclude the last index n from consideration of the min index i - because we cannot sell and make a profit if we buy at the very last price! 
+        # The index j is used to scan through indices greater than i and find the max price. If the min index position i changes, then j must be 
+        # re-evaluated. The for loop maximizes profit as defined in the problem - it does not find global mins or maxs!
+        for i in range(0, n - 1):
+            j = i + 1
             if prices[i] < min:
                 min = prices[i]
-            for j in range(i + 1, n):
-                if prices[j] > max:
-                    max = prices[j]
-        
-        profit = max - min
+                max = min # max must be reset with min to ensure that the index position of max does not fall behind min
+            if prices[j] > max:
+                max = prices[j]
+            if profit < max - min:
+                profit = max - min
+
         return profit
 
 def testCases(test_cases, expected_output):
@@ -37,7 +52,6 @@ def testCases(test_cases, expected_output):
             print(case, "Fail")
 
 if __name__ == '__main__':
-    expected_output = [5, 0]
-    test_cases =  [[7,1,5,3,6,4], [7,6,4,3,1]]
-
+    expected_output = [4, 0, 4, 6, 2, 5, 0, 0, 0]
+    test_cases =  [[3,2,6,5,0,3], [1], [0, 1, 2, 3, 4], [3, 5, 1, 7, 0], [2,4,1], [7,1,5,3,6,4], [7,6,4,3,1], [6,6,6], []] 
     testCases(test_cases, expected_output)
