9f1a43ffeac541604156500f0d64031fbe22f7a5
Nate Pierce <nate.pierce@maymobility.com>
2022-05-01 13:29:42 -0400

    Started 297

--- Hard/297. Serialize and Deserialize Binary Tree/serialBST.py
+++ Hard/297. Serialize and Deserialize Binary Tree/serialBST.py
@@ -9,6 +9,7 @@ The number of nodes in the tree is in the range [0, 1E4].
 -1000 <= Node.val <= 1000
 
 """
+import heapq
 
 # Definition for a binary tree node.
 class TreeNode(object):
@@ -25,6 +26,26 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+        if not root:
+            return []
+
+        treeToString = []
+        queue = [root]
+
+        while queue != []:
+            temp = queue[0]
+            queue.remove(temp)
+            treeToString.append(str(temp.val))
+            if temp.left:
+                queue.append(temp.left)
+            else:
+                treeToString.append("None")
+            if temp.right:
+                queue.append(temp.right)
+            else:
+                treeToString.append("None")
+
+        return ",".join(treeToString)
         
 
     def deserialize(self, data):
@@ -33,9 +54,36 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
+        data = data.split(",")
+        root = TreeNode(int(data[0]))
+        queue = [root]
         
+            
+
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# ////////////                       DO NOT INCLUDE BELOW LINES IN LEETCODE SUBMISSION                      ////////////
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+def testCases():
+    root = TreeNode(1)
+    root.left, root.right = TreeNode(2), TreeNode(3)
+    root.right.left = TreeNode(4)
+    root.right.right = TreeNode(5)
+
+    output = [1,2,3,None,None,4,5]
+
+    ser = Codec().serialize(root) 
+    deser = Codec().deserialize(ser)
+        
+    
+def main():
+    testCases()
+
+if __name__ == '__main__':
+    main()
+        
