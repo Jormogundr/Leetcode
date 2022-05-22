779e6ff57b0865e6937eba748e6f4d5d284ddda1
Jormogundr <nathancp93@gmail.com>
2022-05-22 12:42:31 -0400

    forgot to save a file lul xd

--- Easy/704. Binary Search/binarySearch.cpp
+++ Easy/704. Binary Search/binarySearch.cpp
@@ -1,8 +1,18 @@
 #include <vector>
-
+#include <cmath>
 class Solution {
 public:
     int search(std::vector<int>& nums, int target) {
-        
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid;
+
+        while (low <= high) {
+            mid = floor((low + high)/2);
+            if (nums[mid] < target) {low = mid + 1;}
+            else if (nums[mid] > target) {high = mid -1;}
+            else {return mid;}
+        }
+        return -1;
     }
 };
